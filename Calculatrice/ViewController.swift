1d5abe1adedec4f651a90fa8d2ed6b1d51365832
SUP'Internet 09 <supinternet09@macbook-air-de-supinternet-9.local>
2017-01-13 14:25:40 +0100

    Initial Commit

---
+++ Calculatrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculatrice
+//
+//  Created by SUP'Internet 09 on 13/01/2017.
+//  Copyright © 2017 SUP'Internet 09. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
