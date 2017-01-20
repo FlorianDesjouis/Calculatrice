bb113ce213f6048981119642308126331e9d4b48
SUP'Internet 05 <supinternet05@MacBook-Air-de-SUPInternet-7.local>
2017-01-20 13:03:34 +0100

    modified calculatrice with error for now

--- Calculatrice/CalculatorViewController.swift
+++ Calculatrice/CalculatorViewController.swift
@@ -10,27 +10,11 @@ import UIKit
 
 class CalculatorViewController: UIViewController{
     
-    @IBAction func substract(sender: UIButton) {
-        print("-")
-    }
-    @IBAction func addition(sender: UIButton) {
-        print("+")
-        
-    }
     @IBOutlet weak var display: UILabel!
     
     
-    @IBAction func clear(sender: UIButton) {
-        display.text = ""
-    }
+    let calculator = Calculator()
     
-    @IBAction func addtion(sender: UIButton) {
-        
-    }
-    @IBAction func touchNumber(sender: UIButton) {
-        print("User touched button \(sender.tag)")
-        display.text = display.text! + "\(sender.tag)"
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,7 +24,87 @@ class CalculatorViewController: UIViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+}
+//MARK : User Action
+extension CalculatorViewController {
+    
+    @IBAction func Result(_ sender: Any) {
+        print("result")
+        calculator.result(sender.tag)
+    }
+    
+    @IBAction func substract(sender: UIButton) {
+        print("-")
+        calculator.add(.soustraction)
+        display.text = "-"
+    }
+    @IBAction func addition(sender: UIButton) {
+        print("+")
+        calculator.add(.addition)
+        display.text = "+"
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        display.text = ""
+        print("clear")
+    }
+    
+    @IBAction func touchNumber(sender: UIButton) {
+        
+        calculator.add(sender.tag)
+        display.text = String(sender.tag)
+    }
+}
+//MARK : Operation handled
+enum Operator {
+    case addition
+    case soustraction
+}
+//MARK : Calculator Model
+class Calculator {
+    var firstNumber: Int = 0
+    
+    func add(number: Int) {
+        firstNumber = number
+    }
+    
+    func add(operation: Operator) {
+        self.operation = operation
+    }
+    
+    func isCalculating() -> Bool {
+        return operation != nil
+    }
+    
+    func actualDisplay() -> String {
+        return ""
+    }
+    private func addition(first: Int, second: Int) {
+        
+    }
+    
+    private func soustraction() {
+        
+    }
+    
+    private func result(otherNumber: Int) {
+        if let opp = operation {
+            switch operation {
+                case .addition:
+                    addition(firstNumber, second:otherNumber)
+                case .soustraction:
+                    soustraction()
+                case .result:
+                    result()
+                case .clear:
+                    clear()
+            }
+        }
+    }
+    
+    private func clear() {
+        self.operation = nil
+        self.firstNumber = 0
+    }
 }
 
